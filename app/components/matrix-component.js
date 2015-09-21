37bb371b39d84ba4c8f56433f4996583d3e2f0cd
Nicolas De Nayer <denayer.nicolas@gmail.com>
2015-09-20 19:46:57 -0700

    Switch to position div instead of table to display task

--- app/components/matrix-component.js
+++ app/components/matrix-component.js
@@ -1,23 +1,12 @@
 import Ember from 'ember';
 
-var nbRow = 10;
-var nbCellPerRow = 10;
-var rows = [];
-
-for (var y = nbRow; y >= 0; y--) {
-  var cells = [];
-  for (var x = 0; x <= nbCellPerRow; x++) {
-    cells.push({x: x});
-  }
-  rows.push({y: y, cells: cells});
-}
 
 
 export default Ember.Component.extend({
-  rows: Ember.computed('tasks', function() {
-    return rows;
-  }),
   tasks: Ember.computed('tasks', function() {
     return this.get('tasks');
+  }),
+  extremeCoordinates: Ember.computed('extremeCoordinates', function() {
+    return this.get('extremeCoordinates');
   })
 });
