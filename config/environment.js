a07540ec1afb04aa62f484e2443b10f05fbaa1bc
Nicolas De Nayer <denayer.nicolas@gmail.com>
2015-09-23 08:33:36 -0700

    Change name to valoritization

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'matrix',
+    modulePrefix: 'valoritization',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
