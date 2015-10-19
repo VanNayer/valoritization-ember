a31cd6cb5d2851c364bab60ceb0ff890adcf14d3
Nicolas De Nayer <denayer.nicolas@gmail.com>
2015-10-18 21:00:43 -0700

    User feedback

---
+++ app/components/user-feedback.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+export default Ember.TextField.extend({
+  didInsertElement: function () {
+    var fm_options = {
+      position: "left-bottom",
+      name_required: true,
+      message_placeholder: "Type your feedback here.",
+      message_required: true,
+      feedback_url: "/application/feedback",
+      submit_label: "Send",
+      close_on_click_outside: "true",
+      delayed_options: {
+        send_fail: "Sending failed :(.",
+        send_success: "Thanks for your feedback!"
+      }
+    };
+    fm.init(fm_options);
+
+  }
+
+});
+
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -15,11 +15,12 @@ module.exports = function(defaults) {
   // environments, specify an object as the first parameter. That
   // object's keys should be the environment name and the values
   // should be the asset to use in that environment.
-  //
+  app.import('bower_components/feedback_me/js/jquery.feedback_me.js');
+  app.import('bower_components/feedback_me/css/jquery.feedback_me.css');
+
   // If the library that you are including contains AMD or ES6
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-
   return app.toTree();
 };
