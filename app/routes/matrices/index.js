52d80c6e9addd02f8453fb4044ce452b74e83225
Nicolas De Nayer <denayer.nicolas@gmail.com>
2015-10-21 11:21:31 -0700

    HTML5 section and header

--- app/routes/matrices/index.js
+++ app/routes/matrices/index.js
@@ -2,8 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function () {
-    return this.store.filter('matrix', function(matrix) {
-      return matrix.get('shared');
-    });
+    return this.store.find('matrix', { shared: true });
   }
 });
